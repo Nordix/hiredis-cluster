aafa0b84e1c05acd383acb1631fe7b2e36f416b8
Bjorn Svensson <bjorn.a.svensson@est.tech>
2020-12-03 22:02:31 +0100

    Use hi_realloc() from hiredis
    Hiredis uses its own allocator-functions which by default uses libc.
    By using the same design in hiredis-cluster a user can replace the
    allocator for both hiredis and hiredis-cluster at the same time.
    
    Own allocator functions can be enabled by the API:
    `hiredisAllocFuncs hiredisSetAllocators(hiredisAllocFuncs *ha)`
    
    - This commit only covers `realloc()`
    - Making sure that a realloc returning NULL is handled

--- hiutil.h
+++ hiutil.h
@@ -166,12 +166,9 @@ int _uint_len(uint32_t num);
 #define hi_calloc(_n, _s)                                                      \
     _hi_calloc((size_t)(_n), (size_t)(_s), __FILE__, __LINE__)
 
-#define hi_realloc(_p, _s) _hi_realloc(_p, (size_t)(_s), __FILE__, __LINE__)
-
 void *_hi_alloc(size_t size, const char *name, int line);
 void *_hi_zalloc(size_t size, const char *name, int line);
 void *_hi_calloc(size_t nmemb, size_t size, const char *name, int line);
-void *_hi_realloc(void *ptr, size_t size, const char *name, int line);
 
 #define hi_strndup(_s, _n) strndup((char *)(_s), (size_t)(_n));
 
