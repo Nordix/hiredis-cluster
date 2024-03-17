389c493ef8f4121dd1149824e029bb5be9e4c159
Björn Svensson <bjorn.a.svensson@est.tech>
2024-03-17 17:32:10 +0100

    Update hircluster.h
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- hircluster.h
+++ hircluster.h
@@ -69,7 +69,7 @@
 
 /* The non-const connect callback API is not available when:
  *  - using hiredis prior v.1.1.0; or
- *  - built on Windows since hiredis_cluster.def needs to be updated. */
+ *  - built on Windows since hiredis_cluster.def can't have conditional definitions. */
 #if !(HIREDIS_MAJOR >= 1 && HIREDIS_MINOR >= 1) || _WIN32
 #define HIRCLUSTER_NO_NONCONST_CONNECT_CB
 #endif
